79dd2fbd1901181819e3337b5f79ca44435557e9
Charlie Fadness <boy_916@hotmail.com>
2021-04-14 22:07:16 -0700

    trying out js file

---
+++ js/loops.js
@@ -0,0 +1,18 @@
+// var pictures = function () {
+//     let howMany = prompt("How many coding langages do you want to learn? Enter a number: ")
+//     howMuch = Number(howMuch)
+//     if (howMuch < 3) {
+//         return window.alert()
+//     }
+// }
+
+
+
+
+let howMany = prompt("How many coding langages do you want to learn? Enter a number: ")
+    howMuch = Number(howMuch)
+    if (howMuch < 3) {
+        return <img src="https://images.unsplash.com/photo-1609992130215-9c7ae91c64bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80">image</img>
+    }
+
+    
